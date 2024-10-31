106d684e76ac9d287651ae2b08756601cae831b7
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-10-31 19:01:44 +0200

    add a tiny javascript script

---
+++ scripts/day9.js
@@ -0,0 +1,4 @@
+console.log("Hello from Javascript!");
+document.body.style.backgroundColor = "lightblue";
+console.log("The background color of the body has been changed to lightblue!");
+console.log(2+2);
